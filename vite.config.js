8bca55df150c29b2fe59b0f76ca8b2171a704694
Baron Tang <tangtang960225@163.com>
2025-09-09 16:28:42 +0800

    chore: 安装antd、router、redux，配置vite.config文件

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    // host: "",
+    // port: 8080,
+    open: "/",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "src"),
+    },
+  },
   plugins: [react()],
-})
+});
